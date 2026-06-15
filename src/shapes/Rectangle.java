9a121f97e6ff2c43ef737cf00cdd31478f2fc596
Raiyyan Mahmud Alif <69851090+RaiyyanAlif@users.noreply.github.com>
2026-06-15 14:49:54 +0600

    Update Rectangle.java

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,16 +1,13 @@
+package shapes;
+
 public class Rectangle extends Shape {
 
     private double width;
     private double length;
 
-    public Rectangle(String color,
-                     boolean filled,
-                     double width,
-                     double length) {
-
+    public Rectangle(String color, boolean filled, double width, double length) {
         super(color, filled);
-
-        this.width = width;
+        this.width  = width;
         this.length = length;
     }
 
@@ -26,20 +23,14 @@ public class Rectangle extends Shape {
 
     @Override
     public String toString() {
-
-        return "[ Rectangle ]\n"
-                + super.toString() + "\n"
-                + "Width : " + width + "\n"
-                + "Length: " + length + "\n"
-                + String.format("Area      : %.2f", getArea()) + "\n"
-                + String.format("Perimeter : %.2f", getPerimeter());
+        return "[ Rectangle ]\n" +
+               super.toString()  + "\n" +
+               "Width : " + width  + "\n" +
+               "Length: " + length + "\n" +
+               String.format("Area      : %.2f", getArea())      + "\n" +
+               String.format("Perimeter : %.2f", getPerimeter());
     }
 
-    public double getWidth() {
-        return width;
-    }
-
-    public double getLength() {
-        return length;
-    }
+    public double getWidth()  { return width;  }
+    public double getLength() { return length; }
 }
